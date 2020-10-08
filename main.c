d0785a245e9bc2d8b5b4be67e5518acf35dbd73b
rkdms0527 <71059810+rkdms0527@users.noreply.github.com>
2020-10-08 15:53:46 +0900

    2

--- main.c
+++ main.c
@@ -3,17 +3,29 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int starpoint() {
-	int i;
-	for (i=0; i<10; i++)
-		printf("*");	
+int sumTwo(int a, int b)
+{
+	return (a+b);
 }
+
+int get_max(int a, int b)
+{
+	if (a>b)
+	else
+		return b;
+}
+
+int square(int a)
+{
+	return (a*a);
+}
+
+
 
 int main(int argc, char*argv[]) {
 	
-	starpoint();
-	starpoint();
-	starpoint();
-	
+	printf("sumTwo : %i\n", sumTwo(1,2));
+	printf("get_max : %i\n", get_max(4,6));
+	printf("square : %i\n", square(7));	
 	return 0 ;
 }
