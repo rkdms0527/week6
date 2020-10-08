203019e6f83cc78127bb958d82a6ca69b8e91400
rkdms0527 <71059810+rkdms0527@users.noreply.github.com>
2020-10-08 16:04:12 +0900

    3

--- main.c
+++ main.c
@@ -3,29 +3,14 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int sumTwo(int a, int b)
-{
-	return (a+b);
-}
-
-int get_max(int a, int b)
-{
-	if (a>b)
-	else
-		return b;
-}
-
-int square(int a)
-{
+int square(int a){
 	return (a*a);
 }
 
-
-
 int main(int argc, char*argv[]) {
+	int a = 2;
+	a=square(a);
+	printf("a=%i\n",a);
 	
-	printf("sumTwo : %i\n", sumTwo(1,2));
-	printf("get_max : %i\n", get_max(4,6));
-	printf("square : %i\n", square(7));	
 	return 0 ;
 }
